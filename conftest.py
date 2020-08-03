93b47bc36b370f8aba59ec7c49aa795ce258d85e
Kate <kate.pastbina@dev-pro.net>
2020-08-03 10:38:08 +0300

    add dependency of failure test execution into conftest

--- conftest.py
+++ conftest.py
@@ -3,15 +3,18 @@ import pytest
 from selenium import webdriver
 
 from Pages.base_page import BasePage
+from Pages.login_page import LoginPage
 
 
 @pytest.fixture()
-def browser():
+def browser(request):
     driver = webdriver.Chrome(executable_path='/Users/Kate/PycharmProjects/PracticePageAQA/driver/chromedriver 2')
     driver.maximize_window()
+    failed_before = request.session.testsfailed
     yield driver
-    driver.save_screenshot("/Users/Kate/PycharmProjects/PracticePageAQA/screenshots/screenschot_%s.png" %
-                                      (datetime.datetime.now()))
+    if request.session.testsfailed != failed_before:
+        driver.save_screenshot("/Users/Kate/PycharmProjects/PracticePageAQA/screenshots/screenschot_%s.png" %
+                               (datetime.datetime.now()))
     driver.quit()
 
 
@@ -25,7 +28,6 @@ def open_site(browser):
 # @pytest.fixture()
 # def open_login_page(open_site):
 #     login_page = LoginPage(open_site)
-#     login_page.open_login_page()
 #     login_page.set_login_credentials()
 #     login_page.click_login_btn()
 #     yield open_site
