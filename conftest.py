12d667a3420b8af6cbcf3c70c4ad32542bb43150
kateryna.pastbina <kateryna.pastbina@e-hps.com>
2021-12-01 16:43:46 +0200

    added conftest.py

---
+++ conftest.py
@@ -0,0 +1,39 @@
+import datetime
+import pytest
+from selenium import webdriver
+
+from practice_page.Pages.base_page import BasePage
+
+
+@pytest.fixture()
+def browser(request):
+    path = '/driver/chromedriver'
+    driver =\
+        webdriver.Chrome(executable_path=path)
+    driver.maximize_window()
+    failed_before = request.session.testsfailed
+    yield driver
+    if request.session.testsfailed != failed_before:
+        driver.save_screenshot("/Users/Kate/PycharmProjects/PracticePageAQA/screenshots/screenschot_%s.png" %
+                               (datetime.datetime.now()))
+    driver.quit()
+
+
+@pytest.fixture()
+def open_site(browser):
+    base_page = BasePage(browser)
+    base_page.open_url()
+    yield browser
+
+
+@pytest.fixture()
+def open_course_site(browser):
+    base_page = BasePage(browser)
+    base_page.open_url(url=BasePage.COURSES_URL)
+    yield browser
+
+
+
+
+
+
