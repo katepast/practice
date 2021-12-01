d42a38ed8eaa43b9eb4fdad6642a6996b191f693
kateryna.pastbina <kateryna.pastbina@e-hps.com>
2021-12-01 16:40:18 +0200

    updated assertion message

--- tests/test_search_course.py
+++ tests/test_search_course.py
@@ -11,4 +11,4 @@ class TestSearchCourse:
         main_page.search_course(self.searched_course)
         all_items = main_page.get_all_items()
         print(all_items)
-        assert self.searched_course in all_items, f"Course with name '{self.searched_course}' is absent"
+        assert self.searched_course in all_items, f"Course with name '{self.searched_course}' is absent on the page"
